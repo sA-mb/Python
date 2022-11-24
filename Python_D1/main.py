0286937aea7837da9ec576bb7e378d3a13297485
Sebastián Alejandro Mendoza Banda <smb1896@gmail.com>
2022-11-23 20:06:25 -0500

    correccion 2

--- Python_D1/main.py
+++ Python_D1/main.py
@@ -3,4 +3,4 @@ x = input("What's name of the vity you grew up in?")
 y = input("What's  your pet's name?")
 c= x+" "+y
 
-print(f"Your band name could be {x,y}")
+print(f"Your band name could be {c}")
