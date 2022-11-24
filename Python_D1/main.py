541c245c301ea14eab8023d2b473c462fff97e68
Sebastián Alejandro Mendoza Banda <smb1896@gmail.com>
2022-11-23 20:05:46 -0500

    correccion 1

--- Python_D1/main.py
+++ Python_D1/main.py
@@ -1,5 +1,6 @@
 print("Welcome to the band generator")
 x = input("What's name of the vity you grew up in?")
 y = input("What's  your pet's name?")
+c= x+" "+y
 
 print(f"Your band name could be {x,y}")
